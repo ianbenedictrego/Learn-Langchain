cb5ea17881bdd4e36e52c60d78b48cff25cbb3a6
Ian <ianbenedictrego@gmail.com>
2026-02-23 15:15:47 +0100

    Initial commit - Learn LangChain

---
+++ ChatModels/chatmodel_api.py
@@ -0,0 +1,16 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen3.5-397B-A17B",
+    task = "text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+)
+
+model = ChatHuggingFace(llm= llm)
+result = model.invoke("What is the capital of India")
+
+print(result.content)
